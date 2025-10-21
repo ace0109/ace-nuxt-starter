d6768dccb642d67da97969aa00eb3009fc1ac0a8
Ace <caiyuan0109@gmail.com>
2025-10-21 19:05:58 +0800

    refactor: 支持 i18n 路由前缀的 console layout 设置

--- app/middleware/console-layout.global.ts
+++ app/middleware/console-layout.global.ts
@@ -1,6 +1,9 @@
 export default defineNuxtRouteMiddleware((to) => {
   // 为所有 /console 开头的路由自动设置 console layout
-  if (to.path.startsWith('/console')) {
+  // 支持 i18n 路由前缀: /console 或 /{locale}/console
+  const pathWithoutLocale = to.path.replace(/^\/(en|zh_cn)/, '')
+
+  if (to.path.startsWith('/console') || pathWithoutLocale.startsWith('/console')) {
     setPageLayout('console')
   }
 })
